cd37708eed6cbfadb3edd0d2b25ec04856b3bcc6
BradMW <bodell94@yahoo.com>
2022-01-14 16:36:33 -0600

    removed phone number on portfolio and played with a few css things

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const styles = {
     footer: {
         display: "flex",
         flexWrap: "wrap",
-        justifyContent: "space-evenly",
+        justifyContent: "space-around",
         paddingTop: "10px",
         paddingBottom: "10px",
         background: "#35478C",
@@ -15,7 +15,6 @@ const styles = {
         padding: "2px",
         display: "inline",
         fontSize: "20px",
-        color: "white"
     }
 }
 
@@ -24,7 +23,7 @@ export default function Footer() {
     return(
         <div>
             <ul style={styles.footer}>
-                <li style={styles.li}>555-555-5555</li>
+                {/* <li style={styles.li}>555-555-5555</li> */}
                 <li style={styles.li}><a href="#">bodell94@yahoo.com</a></li>
                 <li style={styles.li}><a href="https://github.com/BradMW" rel="noreferrer">GitHub</a></li>
                 <li style={styles.li}><a href="https://www.linkedin.com/in/bradley-o-dell-b3857b96/" rel="noreferrer">Linkdin</a></li>
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -26,7 +26,7 @@ export default function About() {
   {/* <img src="../../../me.jpg" className="card-img-top" alt=""/> */}
   <img src={require(`../../images/me.jpg`).default}/>
   <div className="card-body">
-    <p className="card-text">My name is Bradley and I am a junior web developer. A few months ago I started my journey into the coding field and have learned so much. I now understand whats behind the screen and how it works. I met a lot of friends and am very excited to see what the future holds!</p>
+    <p className="card-text">My name is Bradley and I am a junior web developer. I have wanted to learn how to code for a while now and thanks to life opening up for me I was able to take a coding bootcamp and finally do something I love. I worked business sales for a long while and I came to love technology through that. I have also worked with UAT teams to make sure that new website features and options were feature ready for users to enjoy. This pandemic has been very tough but it has showed me what is important and it has helped to follow my dream. I spend my free time drawing or flying my drone and editing videos, and now that I have learned to code, I can turn my ideas into reality and showcase my skills. I am planning on learning C# to create a vr experience as I am a vr enthusiest. I want to thank you for checking out my portfolio and I am happy to connect if you have any questions!</p>
   </div>
             </div>
         </div>    
