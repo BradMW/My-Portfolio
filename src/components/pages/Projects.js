fd4558397ae293aae007a81c12aa65c23d348b23
BradMW <bodell94@yahoo.com>
2022-02-04 13:07:31 -0600

    added list of technologies to my projects to save viewing time

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -19,42 +19,42 @@ const styles = {
 const projectInfo = [
     {
         name: "In The Loop",
-        description: "The third group project I worked on. This is a workbook designed for mobile devices. In the loop help users learn how to use for loops.",
+        description: "This is a workbook designed for mobile devices. In the loop help users learn how to use for loops. Built using React, Express, GraphQL, Bcrypt and MUI.",
         imgName: "in-the-loop",
         altTxt: "This is a screen shot of the login page.",
         link: "https://intheloop-app.herokuapp.com/"
     },
     {
         name: "Leverage",
-        description: "This is the second project I worked on. This is a fitness tracker that helps users keep track of the workouts they have done. ",
+        description: "This is a fitness tracker that helps users keep track of the workouts they have done. Built using HandleBars, MySQL, Bcrypt, Express and Tailwind CSS.",
         imgName: "leverage",
         altTxt: "this is the overview page that displays the workouts and days of the week.",
         link: "https://leveragewo.herokuapp.com/"
     },
     {
         name: "FanJam",
-        description: "This is the first project I worked on. FanJam is an app that lets you search up your favorite artist and see the lyrics to their songs as well as see any upcoming events featuring them!",
+        description: "FanJam is an app that lets you search up your favorite artist and see the lyrics to their songs as well as see any upcoming events featuring them! Built using JavaScript, HTML5, and CSS3.",
         imgName: "fanjam",
         altTxt: "This is a screen shot of the FanJam home page.",
         link: "https://bradmw.github.io/FanJam/"
     },
     {
         name: "PWA Budget Tracker",
-        description: "This is one of the homeworks I worked on in my full stack class. This budget tracker has offline functionality so you can keep track of purchases even with no internet.",
+        description: "This budget tracker has offline functionality so you can keep track of purchases even with no internet connection. Built using JavaScript, PWA, Service-Worker, Express, and MongoDB.",
         imgName: "pwa-budget",
         altTxt: "This is the landing page for the budget tracker.",
         link: "https://pwa-budget-tracker-bo.herokuapp.com/"
     },
     {
         name: "Team Profile Generator",
-        description: "This app allows you to create cards for your employees. When you create a new employee you can choose their role and add their email as well as their GitHub link.",
+        description: "This app allows you to create cards for your employees. When you create a new employee you can choose their role and add their email as well as their GitHub link. Built Using JavaScripts, Jest, and Inquirer.",
         imgName: "team-profile",
         altTxt: "This is an overview page displaying what the employee cards will look like together.",
         link: "https://bradmw.github.io/HW-Team-Profile-Gen-BO/"
     },
     {
         name: "ORM E-commerce backend",
-        description: "This app uses SQL and allows users to list items as well as delete them off the store page.",
+        description: "This app uses SQL and allows users to list items as well as delete them off the store page. Built with React, MySQL, Express, and Sequelize.",
         imgName: "orm-back",
         altTxt: "This is a screenshot of the backend running",
         link: "https://drive.google.com/file/d/1jSH02Vy_bd_ZFho6GUi71A3-0BOQiAsq/view"
